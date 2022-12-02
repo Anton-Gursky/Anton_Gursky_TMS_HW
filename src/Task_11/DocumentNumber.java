9fb6fc6d4d545b7798eba94dfb20273f463164a3
Anton Gursky <a.gursky@gmail.com>
2022-12-02 22:42:29 +0300

    modified

--- src/Task_11/DocumentNumber.java
+++ src/Task_11/DocumentNumber.java
@@ -4,17 +4,52 @@ public class DocumentNumber {
 
     public String string;
     public static String[] words;
+    public static char[] letters;
+    public static StringBuilder stringBuilder;
 
     public DocumentNumber(String string) {
         this.string = string;
     }
 
+    // xxxx-yyy-xxxx-yyy-xyxy, где x — это цифра, а y — это буква.
     //Выводим на экран в одну строку два первых блока по 4 цифры
     public static void printFirstTwoDigitBlocks(String string){
+        words = string.split("-");
+        System.out.println(words[0] + words[2]);
+    }
+
+    //Выводим на экран номер документа, но блоки из трех букв заменяем на *** (каждая буква заменятся на *)
+    public static void changeDigitBlocksToStars(String string){
 
         words = string.split("-");
         for (int i = 0; i < words.length; i++){
-            System.out.println(words[i]);
+            if (i % 2 == 0){
+                System.out.print(words[i]);
+            }
+            else {
+                System.out.print("***");
+            }
         }
+        System.out.println();
+    }
+
+    //Выводим на экран только одни буквы из номера документа в формате yyy/yyy/y/y в нижнем регистре.
+    public static void printLettersToLowerCase(String string){
+
+        letters = string.replaceAll("[^a-zA-Z]", "").toLowerCase().toCharArray();
+        for (int i = 0; i < letters.length; i++){
+            System.out.print(letters[i]);
+
+            if(i == 2 || i == 5 || i == 6){
+                System.out.print("/");
+            }
+        }
+    }
+
+    //Выводим на экран буквы из номера документа в формате "Letters:yyy/yyy/y/y" в верхнем регистре (реализовать с помощью класса StringBuilder)
+    public static void printLettersToUpperCase(String string){
+
+        stringBuilder = new StringBuilder(string);
+
     }
 }
--- src/Task_11/Main.java
+++ src/Task_11/Main.java
@@ -5,8 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
         // xxxx-yyy-xxxx-yyy-xyxy, где x — это цифра, а y — это буква.
-        DocumentNumber documentNumber = new DocumentNumber("1234-abc-5678-def-9g10h");
+        DocumentNumber firstDocumentNumber = new DocumentNumber("1234-Abc-5678-dEf-9g10h");
+        DocumentNumber secondDocumentNumber = new DocumentNumber("5555-aaa-5555-aaa-5a5a");
 
-        DocumentNumber.printFirstTwoDigitBlocks(documentNumber.string);
+        DocumentNumber.printFirstTwoDigitBlocks(firstDocumentNumber.string);
+        DocumentNumber.changeDigitBlocksToStars(firstDocumentNumber.string);
+        DocumentNumber.printLettersToLowerCase(firstDocumentNumber.string);
+        DocumentNumber.printLettersToUpperCase(firstDocumentNumber.string);
     }
 }
